33126c1562cb4c4bcfdd61a5ffb4431b3159b2ee
Paul Philippe <pouffy@globulebleu.com>
2015-10-12 14:30:49 +0200

    local project to github

---
+++ js/suffer.js
@@ -0,0 +1,33 @@
+$("#randomVid").click(function(){
+    var videos = $("input:checkbox:checked").map(function(){
+        return this.value;
+    }).toArray();
+    
+    var video = videos[Math.floor(Math.random()*videos.length)];
+
+    if (video != null) {
+    	$('#painshake').text(video);
+        $('body').attr('class','result');
+    }
+    else {
+    	$('body').attr('class','error');
+    }
+
+    localStorage["videos"] = JSON.stringify(videos);
+  })
+
+$(window).load(function() {
+	if (localStorage["videos"] != null) {
+	    var stored = JSON.parse(localStorage["videos"]);
+
+	    for (i=0; i!=stored.length;i++) {
+	        var checkbox = $("input[type='checkbox'][value='"+stored[i]+"']");
+	        checkbox.attr("checked","checked");
+	    }
+	}
+
+});
+
+$("#reset").click(function(){
+    $('body').attr('class','choose');
+})
