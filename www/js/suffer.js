55b726a4f0ceef8c7e4c47598ba9974d198aff45
Paul Philippe <pouffy@globulebleu.com>
2015-10-14 09:10:43 +0200

    adding orders

--- www/js/suffer.js
+++ www/js/suffer.js
@@ -15,7 +15,7 @@ $(".randomVid").click(function(){
         return this.value;
     }).toArray();
         
-    var orders = ["GvA orders you to do -painshake-", "Do -painshake- or get flogged by minions", "Fluffy is hungry, do -painshake- or else", "Don't be a wretched, do -painshake-", "Do -painshake- to fight your inner couchlandrian"];
+    var orders = ["GvA orders you to do -painshake-", "Do -painshake- or get flogged by minions", "Fluffy is hungry, do -painshake- or else", "Don't be a wretched, do -painshake-", "Do -painshake- and beat your inner couchlandrian", "Minions ask nicely: Do -painshake- or get flogged", "A couchlandrian says that you can't do -painshake- prove him wrong !", "Do -painshake- and suffer, suffer, suffer"];
     
     var video = videos[Math.floor(Math.random()*videos.length)];
     var order = orders[Math.floor(Math.random()*orders.length)]; 
